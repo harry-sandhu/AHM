fff3c5b216c0e9c0acc170c6616c6bd81d719771
Harcharan Singh <bcs_2021028@iiitm.ac.in>
2024-04-18 09:34:42 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Hash from "./auth/hashcheckfile";
+import ExcelSheet from "./ExcelSheet";
+const App = () => {
+  const [authenticated, setAuthenticated] = useState(false);
 
-function App() {
+  const handleAuthentication = () => {
+    setAuthenticated(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App font-roboto box-border m-0 p-0">
+      {!authenticated ? (
+        <Hash handleAuthentication={handleAuthentication} />
+      ) : (
+        <ExcelSheet />
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/auth/hashcheckfile.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+const storedHashValue = "A@!34#4";
+
+const checkHash = (inputValue, handleAuthentication) => {
+  if (inputValue === storedHashValue) {
+    console.log("Hash matches! Opening Excel sheet...");
+    handleAuthentication();
+  } else {
+    console.log("Hash mismatch. Please try again.");
+  }
+};
+
+const Hash = ({ handleAuthentication }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      checkHash(inputValue, handleAuthentication);
+    }
+  };
+
+  return (
+    <form className="flex justify-center items-center h-screen max-w-10">
+      <div className="relative">
+        <input
+          type="text"
+          placeholder="Enter Hash Value"
+          value={inputValue}
+          onChange={handleChange}
+          onKeyPress={handleKeyPress}
+          className="mb-4 w-full p-3 border rounded outline-none"
+        />
+        <button
+          className="w-10 bg-blue-500 text-white py-2 rounded"
+          onClick={() => checkHash(inputValue, handleAuthentication)}
+        >
+          Check Hash
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Hash;
